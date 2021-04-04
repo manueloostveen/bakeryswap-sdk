d255462b69486794e208c2f8fb824b1698fef3a2
Nic Smith <thesonicsmith@gmail.com>
2021-04-04 13:59:31 +1200

    Update constants.ts
    Converted FACTORY_ADDRESS to BakerySwap

--- src/constants.ts
+++ src/constants.ts
@@ -19,7 +19,7 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0xBCfCcbde45cE874adCB698cC183deBcF17952812'
+export const FACTORY_ADDRESS = '0xCDe540d7eAFE93aC5fE6233Bee57E1270D3E330F'
 
 export const INIT_CODE_HASH = '0xd0d4c4cd0848c93cb4fd1f498d7013ee6bfb25783ea21593d5834f5d250ece66'
 
